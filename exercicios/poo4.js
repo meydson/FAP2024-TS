c0a7da2acb441e03b69b25a346afb6d3b8fcf129
meydson <64757536+meydson@users.noreply.github.com>
2024-09-18 20:10:23 -0300

    adicionado exercicio 4

---
+++ exercicios/poo4.js
@@ -0,0 +1,35 @@
+var Bra = /** @class */ (function () {
+    function Bra() {
+        this.Numero = Bra.Controle;
+        Bra.Controle += Bra.Controle;
+        this.Saldo = 1000;
+    }
+    Bra.prototype.Sacar = function (valor) {
+        if (valor > this.Saldo) {
+            console.log("Saldo insuficiente, voc\u00EA s\u00F3 possui R$ ".concat(this.Saldo, " dispon\u00EDvel em conta!"));
+        }
+        else {
+            this.Saldo -= valor;
+            console.log("O novo saldo da conta \u00E9 de R$ ".concat(this.Saldo, "!"));
+        }
+    };
+    Bra.prototype.Depositar = function (valor) {
+        if (valor > 1000) {
+            console.log("Valor informado para depósito excede o limite de R$ 1000!");
+            return;
+        }
+        else
+            (valor > 0);
+        this.Saldo += valor;
+        console.log("O novo saldo \u00E9 de R$ ".concat(this.Saldo));
+    };
+    Bra.Controle = 0;
+    return Bra;
+}());
+var novaBra = new Bra;
+function ExibirConta() {
+    console.log(novaBra);
+}
+function ExibirSaldo() {
+    console.log("O saldo atual da conta \u00E9 de R$ ".concat(novaBra.Saldo));
+}
---
+++ exercicios/poo4.ts
@@ -0,0 +1,48 @@
+interface Conta {
+    Numero: number;
+    Saldo: number;
+    Poupanca?: number;
+    Render?(): void;
+    Sacar(valor: number): void;
+    Depositar(valor: number): void;
+}
+
+class Bra implements Conta {
+    static Controle: number = 0;
+    Numero: number;
+    Saldo: number;
+
+    constructor() {
+        this.Numero = Bra.Controle;
+        Bra.Controle += Bra.Controle;
+        this.Saldo = 1000;
+    }
+
+    Sacar(valor: number) {
+        if (valor > this.Saldo) {
+            console.log(`Saldo insuficiente, você só possui R$ ${this.Saldo} disponível em conta!`);
+        } else {
+            this.Saldo -= valor;
+            console.log(`O novo saldo da conta é de R$ ${this.Saldo}!`);
+        }
+    }
+
+    Depositar(valor: number) {
+        if (valor > 1000) {
+            console.log("Valor informado para depósito excede o limite de R$ 1000!")
+            return
+        } else (valor > 0);
+            this.Saldo += valor;
+            console.log(`O novo saldo é de R$ ${this.Saldo}`)
+    }
+}
+
+let novaBra = new Bra;
+
+function ExibirConta() {
+    console.log(novaBra);
+}
+
+function ExibirSaldo() {
+    console.log(`O saldo atual da conta é de R$ ${novaBra.Saldo}`)
+}
